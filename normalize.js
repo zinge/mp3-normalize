ee78ce1a3827700bb2a77ff28613fdd29a4e6805
Semyon Zingelshuher <s.a.zingelshuher@gmail.com>
2020-08-25 09:14:04 +0300

    Create normalize.js

---
+++ normalize.js
@@ -0,0 +1,85 @@
+const { resolve, dirname, basename, extname } = require("path");
+const { readdir, mkdir } = require("fs").promises;
+const { existsSync } = require("fs");
+const { spawn } = require("child_process");
+
+const INPUT_DIR = "/home/zinge/Музыка";
+const OUT_DIR = "/home/zinge/out";
+
+async function getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  const files = await Promise.all(
+    dirents.map((dirent) => {
+      const res = resolve(dir, dirent.name);
+      return dirent.isDirectory() ? getFiles(res) : res;
+    })
+  );
+
+  return Array.prototype.concat(...files);
+}
+
+async function createNewFileDirectory(newDirName) {
+  if (!existsSync(newDirName)) {
+    try {
+      const newDir = await mkdir(newDirName, { recursive: true });
+      return newDir;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+function decode(currentFile, newFile) {
+  const encode = spawn("ffmpeg", [
+    "-i",
+    currentFile,
+    "-af",
+    "loudnorm=I=-16:TP=-1:LRA=18",
+    newFile,
+  ]);
+
+  encode.stderr.on("data", (data) => {
+    console.error(`${data}`);
+  });
+
+  const promise = new Promise((resolve, reject) => {
+    encode.on("error", reject);
+
+    encode.on("exit", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        const err = new Error(`Encode ${newFile} exited with code ${code}`);
+        reject(err);
+      }
+    });
+  });
+
+  return promise;
+}
+
+async function processArray(array) {
+  for (const item of array) {
+    if (extname(item).toLowerCase() === ".mp3") {
+      const dirName = dirname(item);
+      const newDirName = dirName.replace(INPUT_DIR, OUT_DIR);
+      await createNewFileDirectory(newDirName);
+
+      const newFile = newDirName + "/" + basename(item);
+      if (!existsSync(newFile)) {
+        await decode(item, newFile);
+      }
+    }
+  }
+}
+
+async function main() {
+  console.log("Files hierarchy create started");
+  const files = await getFiles(INPUT_DIR);
+  console.log("Files hierarchy created");
+
+  await processArray(files);
+  console.log("All Done!");
+}
+
+main();
